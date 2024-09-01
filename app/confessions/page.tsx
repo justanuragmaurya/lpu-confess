26a3d23b25f3ac2db30e8f6f1a880ad8b6b1872e
justanuragmaurya <justanuragmaurya@gmail.com>
2024-09-01 17:48:37 +0530

    caching fix try 2

--- app/confessions/page.tsx
+++ app/confessions/page.tsx
@@ -1,4 +1,7 @@
 import Confessions from "@/components/Confessions"
+
+export const dynamic = 'force-dynamic';
+
 export default function ConfessionsPage(){
     return(
         <div className="flex gap-5 p-5 justify-center items-center">
