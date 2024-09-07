55ad6759ca08e78a1f135b780833b670d5373287
justanuragmaurya <justanuragmaurya@gmail.com>
2024-09-07 12:51:45 +0530

    caching fex try 100+

--- app/api/confessions/route.ts
+++ app/api/confessions/route.ts
@@ -1,9 +1,12 @@
 import { NextRequest } from "next/server";
 import { PrismaClient } from '@prisma/client';
+import { cookies } from "next/headers";
 
 const client = new PrismaClient();
+const _ = cookies()
 
 export async function GET(req: NextRequest) {
+    
     const data = await client.confessions.findMany();
     return new Response(JSON.stringify(data), {
         headers: {
